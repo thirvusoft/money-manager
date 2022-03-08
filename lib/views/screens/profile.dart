cb5b515b333f681031371675a9608a66c0de7fd6
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2022-03-08 18:26:47 +0530

    customize and profile api connected

--- lib/views/screens/profile.dart
+++ lib/views/screens/profile.dart
@@ -1,13 +1,50 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 import 'dart:math' as math;
-
+import 'dart:async';
 import 'package:money_manager/views/screens/loginScreen.dart';
 import 'package:money_manager/views/screens/splash_screen.dart';
 
-class ProfilePage extends StatelessWidget {
-  TextStyle _style() {
-    return TextStyle(fontWeight: FontWeight.bold);
+class Profiles extends StatefulWidget {
+  Profiles({Key? key}) : super(key: key);
+  @override
+  _ProfilesState createState() => _ProfilesState();
+}
+
+class _ProfilesState extends State<Profiles> {
+  String? full_name;
+  bool isLoading = true;
+  String? name;
+  String? mail;
+  // void main() async {
+  //   await checkupdateNeeded();
+  // }
+  @override
+  void initState() {
+    checkupdateNeeded();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  checkupdateNeeded() async {
+    await profile();
+    login();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    full_name = prefs.getString('mobile_number');
+    name = prefs.getString("full_name");
+    mail = prefs.getString("email");
+    setState(() => isLoading = true);
+    print(full_name);
+    print(mail);
+    print('test');
   }
 
   @override
@@ -20,13 +57,13 @@ class ProfilePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              "Vadivelu",
+              full_name ?? '',
             ),
             Divider(),
             SizedBox(
               height: 16,
             ),
-            Text("glenmargon@gmail.com"),
+            Text(mail ?? ''),
             SizedBox(
               height: 16,
             ),
@@ -35,9 +72,15 @@ class ProfilePage extends StatelessWidget {
               height: 16,
             ),
             Text(
-              "5465154541",
+              name ?? '',
+            ),
+            SizedBox(
+              height: 16,
             ),
             Divider(),
+            SizedBox(
+              height: 16,
+            ),
             FlatButton(
               child: Text(
                 'Logout',
@@ -61,6 +104,8 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
+
+void login() {}
 
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
@@ -101,5 +146,23 @@ class MyClipper extends CustomClipper<Path> {
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     return true;
+  }
+}
+
+Future profile() async {
+  print('profile');
+  var response = await http.post(Uri.parse(
+      "http://192.168.24.34:8000/api/method/money_management_backend.custom.py.api.profile?email=barathpalanisamy2002@gmail.com"));
+  print('response');
+  if (response.statusCode == 200) {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("mobile_number",
+        json.decode(response.body)['message']['mobile_number']);
+    prefs.setString(
+        "full_name", json.decode(response.body)['message']['full_name']);
+    prefs.setString("email", json.decode(response.body)['message']['email']);
+    print(prefs.getString("full_name"));
+    print(prefs.getString("mobile_number"));
+    print(prefs.getString("email"));
   }
 }
